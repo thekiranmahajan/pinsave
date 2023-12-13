b50ba7a250319e7065af1b92087b47db6a252fc7
Kiran Mahajan <91893931+thekiranmahajan@users.noreply.github.com>
2023-12-13 11:28:58 +0530

    user model is ready

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,38 @@
-var express = require('express');
-var router = express.Router();
+const mongoose = require("mongoose");
+const plm = require("passport-local-mongoose");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+mongoose.connect("mongodb://127.0.0.1:27017/pinterest-clone");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+    },
+  ],
+  dp: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  fullName: {
+    type: String,
+    required: true,
+  },
 });
 
-module.exports = router;
+userSchema.plugin();
+
+module.exports = mongoose.model("User", userSchema);
