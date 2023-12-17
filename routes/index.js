790349ec7ac2c61fefd5565b1f0dc877cdfef51b
Kiran Mahajan <91893931+thekiranmahajan@users.noreply.github.com>
2023-12-17 15:34:38 +0530

    Added profile pic upload functionality using multer

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ var express = require("express");
 var router = express.Router();
 const userModel = require("./users");
 const passport = require("passport");
-
 const localStrategy = require("passport-local");
 passport.use(new localStrategy(userModel.authenticate()));
+const upload = require("./multer");
 
 const isLoggedIn = (req, res, next) => {
   if (req.isAuthenticated()) return next();
@@ -15,9 +15,30 @@ router.get("/", (req, res) => {
   res.render("index");
 });
 
-router.get("/profile", isLoggedIn, (req, res, next) => {
-  res.render("profile");
+router.get("/profile", isLoggedIn, async (req, res, next) => {
+  const user = await userModel.findOne({
+    username: req.session.passport.user,
+  });
+  res.render("profile", { user });
 });
+
+router.post(
+  "/file-upload",
+  isLoggedIn,
+  upload.single("image"),
+  async (req, res) => {
+    if (!req.file) {
+      return res.status(400).send("No files were uploaded");
+    }
+    const user = await userModel.findOne({
+      username: req.session.passport.user,
+    });
+    user.avatar = req.file.filename;
+    user.save();
+    res.redirect("/profile");
+  }
+);
+
 router.get("/register", (req, res) => {
   res.render("register");
 });
