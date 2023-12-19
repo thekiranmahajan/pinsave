0871010d13432485933c726e41ea9bf223d596bb
Kiran Mahajan <91893931+thekiranmahajan@users.noreply.github.com>
2023-12-19 13:47:30 +0530

    Added Search bar behaviour related functionalities

--- public/javascripts/navbar.js
+++ public/javascripts/navbar.js
@@ -1,5 +1,33 @@
-const links = document.querySelectorAll(".btnlinks");
+const searchBar = document.getElementById("searchBar");
+const searchIcon = document.getElementById("searchIcon");
+const crossMarkIcon = document.getElementById("crossMarkIcon");
 
+const updateIcons = () => {
+  searchBar.classList.remove("placeholder-transparent");
+  if (searchBar.value === "") {
+    searchIcon.classList.remove("opacity-100");
+    searchIcon.classList.add("opacity-0");
+    crossMarkIcon.classList.add("opacity-0");
+  } else {
+    searchIcon.classList.add("opacity-0");
+    crossMarkIcon.classList.remove("opacity-0");
+  }
+};
+crossMarkIcon.addEventListener("click", () => {
+  searchBar.value = "";
+  crossMarkIcon.classList.add("opacity-0");
+});
+searchBar.addEventListener("focus", updateIcons);
+searchBar.addEventListener("input", updateIcons);
+searchBar.addEventListener("blur", () => {
+  if (searchBar.value === "") {
+    searchBar.classList.add("placeholder-transparent");
+    searchIcon.classList.remove("opacity-0");
+  }
+});
+
+
+const links = document.querySelectorAll(".btnlinks");
 const setActive = (link) => {
   links.forEach((e) => {
     e.classList.remove("bg-black", "text-white");
